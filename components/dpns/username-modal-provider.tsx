372306908f2db58bb7e083290cd7ba4be74f26d5
Shomari <nyusternie@sdot.io>
2025-09-01 16:42:15 -0400

    Update username modal.

--- components/dpns/username-modal-provider.tsx
+++ components/dpns/username-modal-provider.tsx
@@ -4,7 +4,7 @@ import { UsernameModal } from './username-modal'
 import { useUsernameModal } from '@/hooks/use-username-modal'
 
 export function UsernameModalProvider() {
-  const { isOpen, identityId, close } = useUsernameModal()
-  
-  return <UsernameModal isOpen={isOpen} onClose={close} customIdentityId={identityId} />
+    const { isOpen, identityId, close } = useUsernameModal()
+
+    return <UsernameModal isOpen={isOpen} onClose={close} customIdentityId={identityId} />
 }
