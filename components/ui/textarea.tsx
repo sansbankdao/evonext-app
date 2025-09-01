de366e4d63f79ea38e88fd454e98161d63f075b8
Shomari <nyusternie@sdot.io>
2025-09-01 17:18:31 -0400

    Review textarea.

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,23 +1,24 @@
-import * as React from "react"
-import { cn } from "@/lib/utils"
+import * as React from 'react'
+import { cn } from '@/lib/utils'
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+    extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <textarea
-        className={cn(
-          "flex min-h-[80px] w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-purple-600 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:bg-gray-900 dark:ring-offset-gray-950 dark:placeholder:text-gray-400 dark:focus-visible:ring-purple-400",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
+    ({ className, ...props }, ref) => {
+        return (
+            <textarea
+                className={cn(
+                    'flex min-h-[80px] w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-purple-600 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:bg-gray-900 dark:ring-offset-gray-950 dark:placeholder:text-gray-400 dark:focus-visible:ring-purple-400',
+                    className
+                )}
+                ref={ref}
+                {...props}
+            />
+        )
+    }
 )
-Textarea.displayName = "Textarea"
+
+Textarea.displayName = 'Textarea'
 
 export { Textarea }
