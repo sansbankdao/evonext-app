196675a39dcd8dd653dba02c3d33c4ce79f819bc
Shomari <nyusternie@sdot.io>
2025-09-01 14:09:23 -0400

    Update exports.

--- lib/services/index.ts
+++ lib/services/index.ts
@@ -1,23 +1,23 @@
 // Export all services from a single entry point
-export { wasmSdkService, getWasmSdk } from './wasm-sdk-service';
-export { identityService } from './identity-service';
-export { dpnsService } from './dpns-service';
-export { profileService } from './profile-service';
-export { postService } from './post-service';
-export { likeService } from './like-service';
-export { followService } from './follow-service';
-export { repostService } from './repost-service';
-export { bookmarkService } from './bookmark-service';
-export { stateTransitionService } from './state-transition-service';
+export { bookmarkService } from './bookmark-service'
+export { dpnsService } from './dpns-service'
+export { followService } from './follow-service'
+export { identityService } from './identity-service'
+export { likeService } from './like-service'
+export { postService } from './post-service'
+export { profileService } from './profile-service'
+export { repostService } from './repost-service'
+export { stateTransitionService } from './state-transition-service'
+export { wasmSdkService, getWasmSdk } from './wasm-sdk-service'
 
 // Export types
-export type { WasmSdkConfig } from './wasm-sdk-service';
-export type { IdentityInfo, IdentityBalance } from './identity-service';
-export type { ProfileDocument, AvatarDocument } from './profile-service';
-export type { PostDocument, PostStats } from './post-service';
-export type { LikeDocument } from './like-service';
-export type { FollowDocument } from './follow-service';
-export type { RepostDocument } from './repost-service';
-export type { BookmarkDocument } from './bookmark-service';
-export type { StateTransitionResult } from './state-transition-service';
-export type { QueryOptions, DocumentResult } from './document-service';
+export type { BookmarkDocument } from './bookmark-service'
+export type { QueryOptions, DocumentResult } from './document-service'
+export type { FollowDocument } from './follow-service'
+export type { IdentityInfo, IdentityBalance } from './identity-service'
+export type { LikeDocument } from './like-service'
+export type { PostDocument, PostStats } from './post-service'
+export type { ProfileDocument, AvatarDocument } from './profile-service'
+export type { RepostDocument } from './repost-service'
+export type { StateTransitionResult } from './state-transition-service'
+export type { WasmSdkConfig } from './wasm-sdk-service'
