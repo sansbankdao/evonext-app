e78e727a6f36d70849919dded63f49e33b9a0a83
Shomari <nyusternie@sdot.io>
2025-09-02 17:17:44 -0400

    Update content security policy.

--- next.config.js
+++ next.config.js
@@ -48,6 +48,7 @@ const nextConfig = {
                             "img-src 'self' data: https: blob:",
                             "font-src 'self'",
                             "connect-src 'self' https: wss: https://44.240.98.102:1443",
+                            "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://scripts.simpleanalyticscdn.com",
                             "worker-src 'self' blob:",
                             "child-src 'self' blob:"
                         ].join('; ')
