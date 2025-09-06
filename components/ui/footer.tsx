23e412732ef69a154f2fc2e411f389f0da2d46d6
Shomari <nyusternie@sdot.io>
2025-09-06 12:12:14 -0400

    Update footer/nav.

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,35 +1,58 @@
 'use client'
 
 import {
+    GiftIcon,
     MagnifyingGlassIcon,
     RssIcon,
     SparklesIcon,
 } from '@heroicons/react/24/outline'
+import { useAuth } from '@/contexts/auth-context'
 
 export function Footer() {
+    const { user, logout } = useAuth()
 
-    return (
-        <footer className="absolute bottom-0 z-30 w-full h-[45px] grid grid-cols-3 lg:hidden gap-0.5 justify-between bg-slate-900 border border-t-stone-700 overflow-hidden">
-            <a href="/posts" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
-                <RssIcon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
-                <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
-                    posts
-                </span>
-            </a>
+    if (user) {
+        return (
+            <footer className="absolute bottom-0 z-30 w-full h-[45px] grid grid-cols-3 lg:hidden gap-0.5 justify-between bg-slate-950 border border-t-stone-700 overflow-hidden">
+                <a href="/posts" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
+                    <RssIcon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
+                    <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
+                        posts
+                    </span>
+                </a>
 
-            <a href="/explore" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
-                <MagnifyingGlassIcon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
-                <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
-                    explore
-                </span>
-            </a>
+                <a href="/explore" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
+                    <MagnifyingGlassIcon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
+                    <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
+                        explore
+                    </span>
+                </a>
 
-            <a href="/remix" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
-                <SparklesIcon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
-                <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
-                    remix
-                </span>
-            </a>
-        </footer>
-    )
+                <a href="/remix" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
+                    <SparklesIcon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
+                    <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
+                        remix
+                    </span>
+                </a>
+            </footer>
+        )
+    } else {
+        return (
+            <footer className="absolute bottom-0 z-30 w-full h-[45px] grid grid-cols-2 lg:hidden gap-0.5 justify-between bg-slate-950 border border-t-stone-700 overflow-hidden">
+                <a href="/explore" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
+                    <MagnifyingGlassIcon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
+                    <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
+                        explore
+                    </span>
+                </a>
+
+                <a href="/claim" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
+                    <GiftIcon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
+                    <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
+                        claim
+                    </span>
+                </a>
+            </footer>
+        )
+    }
 }
