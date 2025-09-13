5503edd523712345880a4e80c23a744be905fd63
Shomari <nyusternie@sdot.io>
2025-09-13 12:54:37 -0400

    Add apps to footer nav.

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -6,6 +6,7 @@ import {
     MagnifyingGlassIcon,
     RssIcon,
     SparklesIcon,
+    Squares2X2Icon,
     WalletIcon,
 } from '@heroicons/react/24/outline'
 import { useAuth } from '@/contexts/auth-context'
@@ -15,7 +16,7 @@ export function Footer() {
 
     if (user) {
         return (
-            <footer className="absolute bottom-0 z-30 w-full h-[45px] grid grid-cols-4 lg:hidden gap-0.5 justify-between bg-slate-950 border border-t-stone-700 overflow-hidden">
+            <footer className="absolute bottom-0 z-30 w-full h-[45px] grid grid-cols-5 lg:hidden gap-0.5 justify-between bg-slate-950 border border-t-stone-700 overflow-hidden">
                 <Link href="/posts" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
                     <RssIcon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
                     <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
@@ -37,6 +38,13 @@ export function Footer() {
                     </span>
                 </Link>
 
+                <Link href="/apps" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
+                    <Squares2X2Icon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
+                    <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
+                        Apps
+                    </span>
+                </Link>
+
                 <Link href="/wallet" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
                     <WalletIcon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
                     <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
@@ -47,11 +55,18 @@ export function Footer() {
         )
     } else {
         return (
-            <footer className="absolute bottom-0 z-30 w-full h-[45px] grid grid-cols-2 lg:hidden gap-0.5 justify-between bg-slate-950 border border-t-stone-700 overflow-hidden">
+            <footer className="absolute bottom-0 z-30 w-full h-[45px] grid grid-cols-3 lg:hidden gap-0.5 justify-between bg-slate-950 border border-t-stone-700 overflow-hidden">
                 <Link href="/explore" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
                     <MagnifyingGlassIcon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
                     <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
                         Explore
+                    </span>
+                </Link>
+
+                <Link href="/apps" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
+                    <Squares2X2Icon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
+                    <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
+                        Apps
                     </span>
                 </Link>
 
