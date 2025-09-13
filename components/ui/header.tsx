92dc09d5024ef0aced0bd4b3a921de69cc772e4f
Shomari <nyusternie@sdot.io>
2025-09-13 12:33:10 -0400

    Handle network status.

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -14,18 +14,20 @@ export function Header() {
         <div className="absolute top-0 z-50 w-screen h-14 sm:h-16 lg:h-[72px] bg-evonext-500 text-white px-4 py-2 text-sm">
             <div className="pr-7 top-2.5 w-full max-w-7xl mx-auto flex items-center justify-between">
                 <Link href="/" className="-mt-2 sm:-mt-2 lg:-mt-1 flex md:flex-col text-sm font-mono tracking-wider items-center">
-                    <span className="px-2 text-4xl sm:text-5xl font-bold inline tracking-wider">
+                    <span className={`px-2 text-4xl sm:text-5xl font-bold inline tracking-wider ${network !== 'mainnet' ? '' : 'pt-3'}`}>
                         EvoNext
 
-                        <span className="w-fit flex ml-40 sm:ml-52 lg:ml-52 -mt-7 sm:-mt-9 lg:-mt-9">
-                            <h3 className="-rotate-90 tracking-tighter text-xs sm:text-sm uppercase text-sky-300 font-bold animate-pulse">
-                                Testnet
-                            </h3>
+                        {network !== 'mainnet' && <>
+                            <span className="w-fit flex ml-40 sm:ml-52 lg:ml-52 -mt-7 sm:-mt-9 lg:-mt-9">
+                                <h3 className="-rotate-90 tracking-tighter text-xs sm:text-sm uppercase text-sky-300 font-bold animate-pulse">
+                                    Testnet
+                                </h3>
 
-                            <h3 className="hidden sm:inline-flex -ml-8 rotate-90 tracking-tighter text-xs sm:text-sm uppercase text-sky-300 font-bold animate-pulse">
-                                Testnet
-                            </h3>
-                        </span>
+                                <h3 className="hidden sm:inline-flex -ml-8 rotate-90 tracking-tighter text-xs sm:text-sm uppercase text-sky-300 font-bold animate-pulse">
+                                    Testnet
+                                </h3>
+                            </span>
+                        </>}
                     </span>
                 </Link>
 
