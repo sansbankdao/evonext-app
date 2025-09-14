cf65e9c90f726d78a9898e4f3f73fb08f39776e3
Shomari <nyusternie@sdot.io>
2025-09-13 23:45:35 -0400

    Update assistant.

--- components/wallet/assistant.tsx
+++ components/wallet/assistant.tsx
@@ -27,29 +27,32 @@ export function WalletAssistant({ isFullScreen }: WalletAssistantProps) {
     }
 
     return (
-        <main className="p-3">
-            <h1 className="text-slate-800 text-7xl font-bold">
-                Dash ID Assistant
+        <main className="relative p-3 flex flex-col gap-4">
+            <h1 className="text-slate-800 text-4xl font-bold tracking-wider">
+                Platform Assistant
             </h1>
 
             <p className="text-xl xl:text-2xl text-zinc-300 text-pretty">
                 A collection of useful tools for everyday use.
             </p>
 
-            {/* <input
-                v-model="query"
-                id="platform-id"
-                type="text"
-                placeholder="Search for a Dash ID"
-                className="px-5 py-3 text-3xl text-slate-900 bg-sky-50 border rounded-xl shadow"
-            /> */}
+            <ul>
+                <li>Search for Platform users</li>
+                <li>Purchase gift cards instantly</li>
+            </ul>
 
-            <button onClick={search} id="btn-find-id" className="w-full sm:w-fit flex text-2xl font-bold tracking-wider bg-sky-200">
-                Find
+            <textarea
+                // v-model="query"
+                placeholder="What can I help you with today?"
+                className="w-full max-w-2xl px-5 py-3 text-xl text-slate-900 bg-sky-50 border rounded-xl shadow"
+            />
+
+            <button onClick={search} className="w-full sm:w-fit px-5 py-2 text-2xl font-bold tracking-wider text-sky-800 bg-sky-200 border-2 border-sky-400 rounded-xl shadow hover:bg-sky-900 hover:text-sky-200">
+                Make Request
             </button>
 
-            <button onClick={Identity.destroy} id="btn-find-id" className="w-full sm:w-fit flex text-2xl font-bold tracking-wider bg-sky-200">
-                LOGOUT
+            <button onClick={Identity.destroy} className="absolute top-5 right-5 w-fit px-5 py-3 text-2xl font-bold tracking-wider text-red-800 bg-red-200 border-2 border-red-400 rounded-xl shadow hover:bg-red-900 hover:text-red-200">
+                SIGN-OUT OF EVONEXT
             </button>
         </main>
     )
