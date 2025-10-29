e4ce8018933adc2f13c230e78fe0db8eb9550d42
Shomari <nyusternie@sdot.io>
2025-10-28 23:15:26 -0400

    Update UI.

--- app/page.tsx
+++ app/page.tsx
@@ -168,13 +168,13 @@ export default function PublicHomePage() {
                             EvoNext
                         </h1>
 
-                        <h2 className="text-2xl text-sky-700 dark:text-sky-300 mb-8 max-w-2xl mx-auto font-bold tracking-widest">
-                            <span className="text-3xl font-bold text-sky-600">F</span>ree and <span className="text-3xl font-bold text-sky-600">F</span>earless
+                        <h2 className="text-3xl text-sky-700 dark:text-sky-300 mb-8 max-w-2xl mx-auto font-bold tracking-widest">
+                            <span className="text-4xl font-extrabold text-sky-600">F</span>ree and <span className="text-4xl font-extrabold text-sky-600">F</span>earless
                         </h2>
 
                         <p className="text-lg/7 text-slate-600 dark:text-slate-400 sm:max-w-2xl mb-8 mx-auto tracking-wider text-pretty">
                             Discover safe and enjoyable spaces to <span className="font-extrabold">Explore. Curate. Share YOUR Truth</span> Fearlessly ✊
-                            <span className="block">secured by Dash Platform v2 🛡️</span>
+                            <span className="px-2 inline-block">secured by Dash Platform v2 🛡️</span>
                         </p>
 
                         <div className="flex gap-4 justify-center">
