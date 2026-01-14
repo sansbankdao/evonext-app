46060d3f81ff5460486f8aaa37d0894dc6f2a56c
Shomari <nyusternie@sdot.io>
2026-01-13 19:38:05 -0500

    Remove footer for guests.

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -54,29 +54,6 @@ export function Footer() {
             </footer>
         )
     } else {
-        return (
-            <footer className="absolute bottom-0 z-20 w-full h-[45px] grid grid-cols-3 lg:hidden gap-0.5 justify-between bg-slate-950 border border-t-stone-700 overflow-hidden">
-                <Link href="/explore" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
-                    <MagnifyingGlassIcon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
-                    <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
-                        Explore
-                    </span>
-                </Link>
-
-                <Link href="/apps" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
-                    <Squares2X2Icon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
-                    <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
-                        Apps
-                    </span>
-                </Link>
-
-                <Link href="/claim" className="group w-full flex flex-col justify-center items-center bg-stone-700 hover:bg-stone-200">
-                    <GiftIcon className="mt-0.5 h-5 text-fuchsia-100 font-bold text-lg tracking-wider uppercase group-hover:text-fuchsia-900" />
-                    <span className="uppercase text-xs font-medium text-fuchsia-300 tracking-wider group-hover:text-fuchsia-900">
-                        Claim
-                    </span>
-                </Link>
-            </footer>
-        )
+        return null
     }
 }
